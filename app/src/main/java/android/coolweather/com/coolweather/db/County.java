02488f16e409ebcd77490be8a56c646dc6dca570
iluhao <luhao_6@foxmail.com>
2017-08-28 15:13:59 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/android/coolweather/com/coolweather/db/County.java
+++ app/src/main/java/android/coolweather/com/coolweather/db/County.java
@@ -6,10 +6,10 @@ import org.litepal.crud.DataSupport;
  * Created by administrator on 2017/8/28.
  */
 
-public class Country extends DataSupport{
+public class County extends DataSupport{
     private int id;
-    private String countryName;
-    private int weatherId;
+    private String countyName;
+    private String weatherId;
     private int cityId;
 
     public int getId() {
@@ -20,19 +20,19 @@ public class Country extends DataSupport{
         this.id = id;
     }
 
-    public String getCountryName() {
-        return countryName;
+    public String getCountyName() {
+        return countyName;
     }
 
-    public void setCountryName(String countryName) {
-        this.countryName = countryName;
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
     }
 
-    public int getWeatherId() {
+    public String getWeatherId() {
         return weatherId;
     }
 
-    public void setWeatherId(int weatherId) {
+    public void setWeatherId(String weatherId) {
         this.weatherId = weatherId;
     }
 
