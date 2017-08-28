6330bc89c49c5936cfe092a895577ff5e33e34a4
iluhao <luhao_6@foxmail.com>
2017-08-28 18:00:47 +0800

    完成天气界面

---
+++ app/src/main/java/android/coolweather/com/coolweather/gson/AQI.java
@@ -0,0 +1,14 @@
+package android.coolweather.com.coolweather.gson;
+
+/**
+ * Created by administrator on 2017/8/28.
+ */
+
+public class AQI {
+    public AQICity city;
+
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
